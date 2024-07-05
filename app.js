0e0efea1f4f361a2b0cceee497319b64702d87ba
Andy Maries <andy.maries@genesys.com>
2024-07-05 14:53:59 +1000

    refactor: Update initiateLogin function and query parameter names in app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import { startSession } from "./sessionHandler.js";
-let platformClientModule = require("platformClient");
-const PlatformClient = platformClientModule.ApiClient.instance;
+
+// Ensure PlatformClient is available globally
+import platformClientModule from "platformClient";
+window.PlatformClient = platformClientModule.ApiClient.instance;
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("{am} window.location:", window.location);
@@ -8,50 +10,52 @@ document.addEventListener("DOMContentLoaded", () => {
   const gc_region = urlParams.get("gc_region");
   const gc_clientId = urlParams.get("gc_clientId");
   const gc_redirectUrl = urlParams.get("gc_redirectUrl");
+  const urlHash = window.location.hash.substr(1);
+  const hashParams = new URLSearchParams(urlHash);
+  const accessToken = hashParams.get("access_token");
 
-  if (!gc_region || !gc_clientId || !gc_redirectUrl) {
-    console.error(
-      "{am} Client ID, region or redirect url not found in URL parameters.",
-      window.location
+  if (gc_region && gc_clientId && gc_redirectUrl) {
+    console.log(
+      "{am} Initial login parameters found in URL. Initiating login.",
+      gc_region,
+      gc_clientId,
+      gc_redirectUrl
     );
-    return;
-  }
-
-  if (window.location.hash) {
-    console.log("{am} Window location has hash.");
-    const hashParams = new URLSearchParams(window.location.hash.substring(1));
-    const accessToken = hashParams.get("access_token");
-
-    if (accessToken) {
-      startSession(accessToken);
-    } else {
-      console.error("{am} Access token not found in URL hash.");
-    }
-  } else {
-    console.log("{am} Initiating login.");
-    // Ensure PlatformClient is available before calling initiateLogin
-    if (PlatformClient) {
+    if (window.PlatformClient) {
       initiateLogin(gc_clientId, gc_region, gc_redirectUrl);
     } else {
       console.error("{am} PlatformClient is not defined.");
       console.error("{am} window", window);
     }
+  } else if (accessToken) {
+    console.log("{am} Redirect after successful login detected");
+    if (!sessionStorage.getItem("gc_access_token")) {
+      sessionStorage.setItem("gc_access_token", accessToken);
+      console.log("{am} Access token stored in sessionStorage");
+      startSession(accessToken);
+    }
+  } else {
+    console.error("{am} Something is really wrong :(");
   }
 });
 
 async function initiateLogin(gc_clientId, gc_region, gc_redirectUrl) {
   console.log("{am} Login initiated.");
   try {
-    console.log("{am} Setting up PlatformClient.", PlatformClient);
-    PlatformClient.setEnvironment(gc_region);
-    PlatformClient.setPersistSettings(true, "_am_");
-    PlatformClient.setReturnExtendedResponses(true);
+    console.log("{am} Setting up PlatformClient.", window.PlatformClient);
+    window.PlatformClient.setEnvironment(gc_region);
+    window.PlatformClient.setPersistSettings(true, "_am_");
+    window.PlatformClient.setReturnExtendedResponses(true);
 
     console.log("%c{am} Logging in to Genesys Cloud", "color: green");
-    await PlatformClient.loginImplicitGrant(gc_clientId, gc_redirectUrl, {});
+    await window.PlatformClient.loginImplicitGrant(
+      gc_clientId,
+      gc_redirectUrl,
+      {}
+    );
 
     // GET Current UserId
-    const uapi = new PlatformClient.UsersApi();
+    const uapi = new window.PlatformClient.UsersApi();
     let user = await uapi.getUsersMe({});
     console.log(user);
 
