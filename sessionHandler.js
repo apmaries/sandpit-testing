ae36db33d1dfea54aee2f0ee636e6f70607c714d
Andy Maries <andy.maries@genesys.com>
2024-07-05 16:24:40 +1000

    refactor: Update startSession function in sessionHandler.js and app.js to use global platformClientModule

--- sessionHandler.js
+++ sessionHandler.js
@@ -3,9 +3,15 @@
 export async function startSession() {
   console.log("{am} Starting session");
 
-  // GET Current UserId
-  //const uapi = new window.platformClientModule.UsersApi();
-  let user = await window.am.usersApi.getUsersMe({});
-  console.log("{am} User details returned", user);
-  document.getElementById("content").innerText = "Welcome, " + user.name + "!";
+  try {
+    // GET Current UserId
+    //const uapi = new window.platformClientModule.UsersApi();
+    let user = await window.am.usersApi.getUsersMe({});
+    console.log("{am} User details returned", user);
+    document.getElementById("content").innerText =
+      "Welcome, " + user.name + "!";
+  } catch (error) {
+    console.error("{am} Error getting user details. ", error);
+    throw error;
+  }
 }
