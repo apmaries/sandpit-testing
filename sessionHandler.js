701d145770540678f6941735ac32e6617d7579bf
Andy Maries <andy.maries@genesys.com>
2024-07-05 15:07:09 +1000

    refactor: Update sessionHandler.js to start session with access token and retrieve current user

--- sessionHandler.js
+++ sessionHandler.js
@@ -1,9 +1,12 @@
 // sessionHandler.js
 import { handleApiCalls } from "./apiHandler.js";
 
-export function startSession(accessToken) {
+export async function startSession(accessToken) {
   sessionStorage.setItem("gc_access_token", accessToken);
   document.getElementById("content").innerText = "Logged in successfully";
 
-  handleApiCalls();
+  // GET Current UserId
+  const uapi = new window.PlatformClient.UsersApi();
+  let user = await uapi.getUsersMe({});
+  console.log(user);
 }
