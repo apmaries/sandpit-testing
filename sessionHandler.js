7224c84488af6c088f8e75e7400822316754f236
Andy Maries <andy.maries@genesys.com>
2024-07-05 15:11:18 +1000

    refactor: Update startSession function in sessionHandler.js

--- sessionHandler.js
+++ sessionHandler.js
@@ -1,12 +1,12 @@
 // sessionHandler.js
 import { handleApiCalls } from "./apiHandler.js";
 
-export async function startSession(accessToken) {
-  sessionStorage.setItem("gc_access_token", accessToken);
+export async function startSession() {
   document.getElementById("content").innerText = "Logged in successfully";
+  const PlatformClient = window.PlatformClient;
 
   // GET Current UserId
-  const uapi = new window.PlatformClient.UsersApi();
+  const uapi = new PlatformClient.UsersApi();
   let user = await uapi.getUsersMe({});
   console.log(user);
 }
