1cd0630bca6ed94b9708b7e0d91ee63c2563f2b3
Andy Maries <andy.maries@genesys.com>
2024-07-05 16:09:23 +1000

    refactor: Update startSession function in sessionHandler.js

--- sessionHandler.js
+++ sessionHandler.js
@@ -1,11 +1,12 @@
 // sessionHandler.js
+let platformClientModule = require("platformClient");
 
 export async function startSession() {
   const PlatformClient = window.PlatformClient;
   console.log("{am} Starting session", PlatformClient);
 
   // GET Current UserId
-  const uapi = new PlatformClient.UsersApi();
+  const uapi = new platformClientModule.UsersApi();
   let user = await uapi.getUsersMe({});
   console.log("{am} User details returned", user);
   document.getElementById("content").innerText = "Welcome, " + user.name + "!";
