71e1b15887936ed0d240332d0386966819b8da0f
Andy Maries <andy.maries@genesys.com>
2024-07-08 15:11:34 +1000

    feat: Initialize test mode and mock APIs with testing data

---
+++ src/core/testManager.js
@@ -0,0 +1,69 @@
+// testManager.js
+// Description: Module for managing test execution
+
+import { applicationConfig } from "./configManager.js";
+
+("use strict");
+
+let t_capi = null;
+let t_oapi = null;
+let t_wapi = null;
+
+// Utility function to fetch test data
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    console.error(`[OFG] Error fetching data from ${url}`, error);
+    throw error; // Rethrow to handle it in the caller function
+  }
+}
+
+// Prrimary function to initialize test mode
+export async function initializeTestMode() {
+  console.log("[OFG] Initializing test mode");
+  console.log("[OFG] App config", applicationConfig);
+
+  const config = applicationConfig.testing;
+
+  // Define mock data promises
+  const outboundAggregatesDataPromise = fetchData(
+    config.outboundAggregatesDataUrl
+  );
+  const campaignsPromise = fetchData(config.campaignsUrl);
+  const businessUnitsPromise = fetchData(config.businessUnitsUrl);
+  const businessUnitSettingsPromise = fetchData(config.businessUnitSettingsUrl);
+  const planningGroupsPromise = fetchData(config.planningGroupsUrl);
+  const inboundFcDataPromise = fetchData(config.inboundFcDataUrl);
+
+  // Assign mock data promises to mock API functions
+  t_capi = {
+    getOutboundConversationsAggregates: function () {
+      return outboundAggregatesDataPromise;
+    },
+  };
+  t_oapi = {
+    getOutboundCampaigns: function () {
+      return campaignsPromise;
+    },
+  };
+  t_wapi = {
+    getBusinessUnits: function () {
+      return businessUnitsPromise;
+    },
+    getBusinessUnitData: function () {
+      return businessUnitSettingsPromise;
+    },
+    getPlanningGroups: function () {
+      return planningGroupsPromise;
+    },
+    getInboundForecastData: function () {
+      return inboundFcDataPromise;
+    },
+  };
+
+  console.log("[OFG] Mock APIs initialized with testing data");
+}
+
+export { t_capi, t_oapi, t_wapi };
