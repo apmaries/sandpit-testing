9737e1acc6cc38dab772b01100e61de3c1c02018
Andy Maries <andy.maries@genesys.com>
2024-07-08 15:12:05 +1000

    feat: Add lazy loading for images in pageHandler module

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // Description: Main application module
 
 import { applicationConfig } from "./core/configManager.js";
+import { loadPageOne } from "./modules/pageHandler.js";
 
 const testMode = applicationConfig.testMode;
 ("use strict");
@@ -10,22 +11,5 @@ export function runApp() {
   console.log("[OFG] Initializing main app");
 
   // Add the logic for the rest of your app here.
-  // You can use the user details passed from the startSession function.
-
-  // Example: Load additional modules or components
-  loadDashboard();
-  setupEventListeners();
-  // more app initialization code...
-}
-
-function loadDashboard() {
-  // Example function to load the dashboard
-  console.log("[OFG] Loading dashboard");
-  // Add your dashboard loading logic here
-}
-
-function setupEventListeners() {
-  // Example function to set up event listeners
-  console.log("[OFG] Setting up event listeners");
-  // Add your event listener setup logic here
+  loadPageOne();
 }
