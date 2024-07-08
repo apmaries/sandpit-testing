e726623a51015dde254a3d5a2384c5b226041641
Andy Maries <andy.maries@genesys.com>
2024-07-08 14:18:58 +1000

    refactor: Update sessionManager.js to get user details and open notifications channel

--- src/core/sessionManager.js
+++ src/core/sessionManager.js
@@ -2,11 +2,38 @@
 // Description: Module for handling session-related logic
 
 import { applicationConfig } from "./configManager.js";
-import { uapi } from "../app.js";
+import { napi, uapi } from "../app.js";
 
 const testMode = applicationConfig.testMode;
 ("use strict");
 
+// Function to get user details
+async function getUser() {
+  try {
+    let user = await uapi.getUsersMe({});
+    return user;
+  } catch (error) {
+    console.error("[OFG] Error getting user details. ", error);
+    throw error;
+  }
+}
+
+// Function to open notification channel
+async function openNotificationsChannel() {
+  let channel = null;
+  try {
+    channel = await napi.postNotificationsChannels();
+  } catch (error) {
+    console.error("[OFG] Error opening notifications channel. ", error);
+    throw error;
+  }
+
+  console.log("[OFG] Notifications channel opened");
+  applicationConfig.notifications.uri = channel.connectUri;
+  applicationConfig.notifications.id = channel.id;
+}
+
+// Primary function to start the session
 export async function startSession() {
   console.log("[OFG] Starting session");
   let appUser = null;
@@ -14,15 +41,8 @@ export async function startSession() {
   if (testMode) {
     appUser = "Test User";
   } else {
-    try {
-      // GET Current UserId
-      let user = await uapi.getUsersMe({});
-      console.log("[OFG] User details returned", user);
-      appUser = user.name;
-    } catch (error) {
-      console.error("[OFG] Error getting user details. ", error);
-      throw error;
-    }
+    user = await getUser();
+    openNotificationsChannel();
   }
   document.getElementById("user-welcome").innerText =
     "Welcome, " + appUser + "!";
