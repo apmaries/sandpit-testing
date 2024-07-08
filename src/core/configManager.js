868585cf7a8410eec36f47c0de9864197a926453
Andy Maries <andy.maries@genesys.com>
2024-07-08 14:19:10 +1000

    feat: Add notifications configuration to applicationConfig

--- src/core/configManager.js
+++ src/core/configManager.js
@@ -2,6 +2,10 @@
 // Description: Centralized configuration module
 
 let applicationConfig = {
+  notifications: {
+    uri: "",
+    id: "",
+  },
   testMode: window.location.protocol !== "https:",
   // Add more configuration options as needed
 };
