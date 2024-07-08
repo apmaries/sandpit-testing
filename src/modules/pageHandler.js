89b502dc451807123ee47373cb7a2e138f71b49d
Andy Maries <andy.maries@genesys.com>
2024-07-08 15:33:25 +1000

    feat: Update pageHandler module to load business units and populate dropdown

--- src/modules/pageHandler.js
+++ src/modules/pageHandler.js
@@ -5,17 +5,43 @@ import { applicationConfig } from "../core/configManager.js";
 import { applicationState } from "../core/stateManager.js";
 import { wapi } from "../app.js";
 import { t_wapi } from "../core/testManager.js";
+import { populateDropdown } from "../utils/domUtils.js";
 
 const testMode = applicationConfig.testMode;
 ("use strict");
 
+// Function to get user details
+async function getUser() {
+  try {
+    let user = await uapi.getUsersMe({});
+    return user;
+  } catch (error) {
+    console.error("[OFG] Error getting user details. ", error);
+    throw error;
+  }
+}
+
 export async function loadPageOne() {
   console.log("[OFG] Loading page one");
   console.debug("[OFG] Application state", applicationState);
+  const businessUnitListbox = document.getElementById("business-unit-listbox");
 
-  if (testMode) {
-    console.log("[OFG] t_wapi", t_wapi);
-  } else {
-    console.log("[OFG] wapi", wapi);
+  // Get list of business units
+  async function getBusinessUnits() {
+    try {
+      const businessUnits = testMode
+        ? await t_wapi.getBusinessUnits()
+        : await wapi.getBusinessUnits();
+      console.log("[OFG] Business units", businessUnits.entities);
+
+      return businessUnits.entities; // Return the list of business units
+    } catch (error) {
+      console.error("[OFG] Error getting business units. ", error);
+      throw error;
+    }
   }
+
+  // Main logic for loading page one
+  const businessUnits = await getBusinessUnits();
+  populateDropdown(businessUnitListbox, businessUnits, "name", true);
 }
