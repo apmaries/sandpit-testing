8f9aa47ea725381782fb86f477c45065139ec5cf
Andy Maries <andy.maries@genesys.com>
2024-07-08 15:11:21 +1000

    refactor: Update applicationConfig.js to include testing URLs

--- src/core/configManager.js
+++ src/core/configManager.js
@@ -7,6 +7,18 @@ let applicationConfig = {
     id: "",
   },
   testMode: window.location.protocol !== "https:",
+  testing: {
+    outboundAggregatesDataUrl:
+      "/outboundForecastGenerator/test/source/outboundAggregateData.json",
+    businessUnitsUrl:
+      "/outboundForecastGenerator/test/source/businessUnits.json",
+    businessUnitSettingsUrl: "/outboundForecastGenerator/test/source/bu.json",
+    planningGroupsUrl:
+      "/outboundForecastGenerator/test/source/planningGroups.json",
+    campaignsUrl: "/outboundForecastGenerator/test/source/campaigns.json",
+    inboundFcDataUrl:
+      "/outboundForecastGenerator/test/source/inboundForecastData.json",
+  },
   // Add more configuration options as needed
 };
 
