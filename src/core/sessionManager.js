136d94e16333fa307778d03f18cbfe7bb1020f36
Andy Maries <andy.maries@genesys.com>
2024-08-26 16:44:25 +1000

    refactor: Update index.html and sessionManager.js for TIL logging

--- src/core/sessionManager.js
+++ src/core/sessionManager.js
@@ -8,7 +8,7 @@ const testMode = applicationConfig.testMode;
 ("use strict");
 
 export async function startSession() {
-  console.log("{am} Starting session");
+  console.log("[TIL] Starting session");
   let appUser = null;
 
   if (testMode) {
@@ -17,10 +17,10 @@ export async function startSession() {
     try {
       // GET Current UserId
       let user = await uapi.getUsersMe({});
-      console.log("{am} User details returned", user);
+      console.log("[TIL] User details returned", user);
       appUser = user.name;
     } catch (error) {
-      console.error("{am} Error getting user details. ", error);
+      console.error("[TIL] Error getting user details. ", error);
       throw error;
     }
   }
